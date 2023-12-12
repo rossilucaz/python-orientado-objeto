05851cf20a4d8b4246ff3c1c6234468e6593e843
rossilucaz <lucasdallagnolr@gmail.com>
2023-12-12 19:35:44 -0300

    .

--- class_file.py
+++ class_file.py
@@ -1,6 +1,17 @@
-class Conta:  
+class Conta: 
+     
     def __init__(self, numero, titular, saldo, limite):
+        print(f'Constuindo o Objeto {self}')
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+        
+    def extrato(self):
+        print(f'Saldo de {self.saldo}, do Titular {self.titular}')
+    
+    def depositar(self, valor):
+        self.saldo += valor
+    
+    def sacar(self, valor):
+        self.saldo -= valor
