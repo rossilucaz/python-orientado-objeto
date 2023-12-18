8b9a33aa1531923dd6af6d7a2e4504473fe053a0
rossilucaz <lucasdallagnolr@gmail.com>
2023-12-18 20:42:57 -0300

    .

--- class_file.py
+++ class_file.py
@@ -2,16 +2,20 @@ class Conta:
      
     def __init__(self, numero, titular, saldo, limite):
         print(f'Constuindo o Objeto {self}')
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         
     def extrato(self):
-        print(f'Saldo de {self.saldo}, do Titular {self.titular}')
+        print(f'Saldo de {self.__saldo}, do Titular {self.__titular}')
     
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+        
+    def tranferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
